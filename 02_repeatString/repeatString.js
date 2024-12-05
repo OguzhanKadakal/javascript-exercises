f5928b32f2ee457ed5c2dd34dec1cce4862d9476
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2024-12-06 00:06:02 +0300

    Completed the first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    let string = "";
+    for (i = 0; i < times; i++) {
+        string += word;
+    }
+    if ( times < 0) {
+        return "ERROR"
+    }
+    return string;
 };
 
 // Do not edit below this line
