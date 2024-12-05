e79c460b8a55fc96f1d78da5dd9f7a3324b4e504
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2024-12-06 02:00:16 +0300

    Completed the 4th and 5th exersices

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(number1,number2) {
+    let total = number1 + number2;
+    if (number1 < 0 || number2 < 0) {
+        return "ERROR"
+    } else if (Number.isInteger(number1 / number2) === false) {
+        return "ERROR"
+    }
+    let sum = 0;
+    for (let i = 1; i < total; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
