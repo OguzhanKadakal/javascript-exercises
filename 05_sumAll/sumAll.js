4adff3fe48031f3518fb2402a326f657bd42dd26
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2024-12-13 03:01:36 +0300

    Completed the excersices from 5 to 8

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,10 @@
 const sumAll = function(number1,number2) {
     let total = number1 + number2;
-    if (number1 < 0 || number2 < 0) {
+    if (number1 < 0) {
         return "ERROR"
-    } else if (Number.isInteger(number1 / number2) === false) {
+    } else if (number2 < 0) {
+        return "ERROR"
+    } else if (!Number.isInteger(total)) {
         return "ERROR"
     }
     let sum = 0;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
-	
+const add = function (number1, number2) {
+  let result = number1 + number2;
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function (number1, number2) {
+  let result = number1 - number2;
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+const myNumbers = array;
+let total = 0;
+myNumbers.forEach(element => {
+  return total += element;
+});
+return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  const myNumbers = array;
+  const total = myNumbers.reduce(multiplyOfArray)
+  function multiplyOfArray (accumulator, element) {
+    return accumulator * element;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function (number1, number2) {
+  let result = Math.pow(number1, number2);
+  return result;
 };
+  
+const factorial = function (number) {
+  if (number === 0) {
+    return 1;
+  };
+  const myNumbers = [];
+  for (i = 1; i < number + 1; i++) {
+    myNumbers.unshift(i);
+  }
+  const total = myNumbers.reduce(multiplyOfArray)
+  function multiplyOfArray (accumulator, element) {
+    return accumulator * element;
+  }
+  return total;
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
@@ -29,5 +54,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
