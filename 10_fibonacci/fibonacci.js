1b9371b02596ea79607e802a1e993c5c646b96ab
OguzhanKadakal <76901135+OguzhanKadakal@users.noreply.github.com>
2024-12-25 17:04:25 +0300

    finished the exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let fib = Number(n);
+    if (fib < 0) {
+        return "OOPS";
+    } else if (fib === 0) {
+        return 0;
+    }
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i<= fib; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 };
 
 // Do not edit below this line
